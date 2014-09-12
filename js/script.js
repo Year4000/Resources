d890bf1ac0b1aec393061dba2faa8c19bad5ba66
Joshua Rodriguez <ewized@gmail.com>
2014-09-11 19:15:37 -0700

    Make side bar slide open and close

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ var toggled = true;
 /** Toggle the sidebar */
 function toggle_sidebar() {
     document.getElementById("bar").style.background = toggled ? "#999" : "#666";
-    document.getElementById("side").style.display = toggled ? "block" : "none";
+    document.getElementById("side").style.left = toggled ? "0px" : "-500px";
     toggled = !toggled;
 }
