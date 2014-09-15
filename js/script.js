1b4f42e71a04c03b5cf621a782500b426dac44f4
Joshua Rodriguez <ewized@gmail.com>
2014-09-15 12:07:26 -0700

    Anonymous function for sidebar

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 var toggled = true;
 
-window.onresize=function () {
+window.onresize = function () {
     if (window.innerWidth > 768) {
         document.getElementById("side").style.left = null;
         document.getElementById("bar").style.background = null;
         toggled = true;
     }
-};
+}
 
 /** Toggle the sidebar */
-function toggle_sidebar() {
+document.getElementById("bar").onclick = function () {
     document.getElementById("bar").style.background = toggled ? "#999" : "#666";
     document.getElementById("side").style.left = toggled ? "0px" : "-500px";
     toggled = !toggled;
